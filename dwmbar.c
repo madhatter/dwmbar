ff1ab36fef4f038a927917c5d7ce9085bc0ff2f0
Arvid Warnecke <madhatter@nostalgix.org>
2013-11-07 16:43:16 +0100

    Hello world greeting in a small window.

--- dwmbar.c
+++ dwmbar.c
@@ -5,13 +5,39 @@
 int main()
 {
 	Display *dpy;
+	Window rootwin;
+	Window win;
+	XEvent e;
+	GC gc;
 
-	if (!(dpy=XOpenDisplay(NULL))) {
+	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "ERROR: could not open display\n");
 		exit(1);
 	}
+	
+	rootwin = RootWindow(dpy, DefaultScreen(dpy));
+	win = XCreateSimpleWindow(dpy, rootwin, 1, 1, 100, 50, 0, 
+		BlackPixel(dpy, DefaultScreen(dpy)), WhitePixel(dpy, DefaultScreen(dpy)));
+	gc = XCreateGC(dpy, win, 0, NULL);
+
+	XSelectInput(dpy, win, ExposureMask|ButtonPressMask);
+	XStoreName(dpy, win, "hello");
+	XSetForeground(dpy, gc, BlackPixel(dpy, DefaultScreen(dpy)));
+
+	XMapWindow(dpy, win);
+
+	while(1) {
+		XNextEvent(dpy, &e);
+		if(e.type == Expose && e.xexpose.count<1)
+			XDrawString(dpy, win, gc, 10, 10, "Hello World!", 12);
+		else if(e.type == ButtonPress) break;
+	}
 
 	printf("Done.\n");
+
+	XFreeGC(dpy, gc);
+	XDestroyWindow(dpy,win);
+	XCloseDisplay(dpy);
 
 	return 0;
 }
