51c4c8770263c5ef092b39b75b89e2c7f7dbe691
Arvid Warnecke <madhatter@nostalgix.org>
2013-11-07 14:46:33 +0100

    Try to connect to the Xserver and open the display.

--- dwmbar.c
+++ dwmbar.c
@@ -1,7 +1,17 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main()
+{
+	Display *dpy;
+
+	if (!(dpy=XOpenDisplay(NULL))) {
+		fprintf(stderr, "ERROR: could not open display\n");
+		exit(1);
+	}
+
+	printf("Done.\n");
 
 	return 0;
 }
