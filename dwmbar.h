83158848ef5cc3bb8e1d4bd75e7fb29c0fae7378
Arvid Warnecke <madhatter@nostalgix.org>
2014-04-02 09:47:55 +0200

    Changes necessary for workstation.

--- dwmbar.h
+++ dwmbar.h
@@ -1,9 +1,9 @@
-#define PACMAN_DB_PATH "/home/madhatter/pacman"
-#define ETHERNET_DEVICE "enp0s25"
+#define PACMAN_DB_PATH "/home/awarnecke/pacman"
+#define ETHERNET_DEVICE "eth0"
 #define ETHERNET_STRING "connected"
 #define WIRELESS_DEVICE "wlp12s0"
 #define OFFLINE_STRING "offline"
 #define BATTERY_NOW "/sys/class/power_supply/BAT0/charge_now"
 #define BATTERY_FULL "/sys/class/power_supply/BAT0/charge_full"
-#define ENABLE_BATTERY 1
-#define ENABLE_WIRELESS 1
+#define ENABLE_BATTERY 0
+#define ENABLE_WIRELESS 0
